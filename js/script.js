c2ba3e7a8e35dcd6fbbc5d962b2a6e55be210321
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-21 15:12:02 +0100

    Add Km calculation price & Discount calculation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Input
-const kilometres = prompt ("Quanti chilometri dovrai percorrere?")
+const kilometres = prompt ("Quanti chilometri dovrai percorrere?");
 
-const personAge = prompt ("Qual'è la tua età?")
+const personAge = prompt ("Qual'è la tua età?");
 
+// Km calculation price
+let totKilometes = kilometres * 0.25;
+
+// Discount calculation
+let ageDiscount;
+
+if (personAge < 18) {
+    ageDiscount = totKilometes * 20 / 100;
+} else if (personAge >= 65){
+    ageDiscount = totKilometes * 20 / 100 ;
+}
+
+
+let totPrice = totKilometes - ageDiscount;
+
+console.log (totKilometes);
+
+console.log (ageDiscount);
